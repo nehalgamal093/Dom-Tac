fa357802250a1fbc1f0d9d12376dbad6cad1ef30
nehalgamal093 <nehalgamal093@gmail.com>
2024-03-04 13:38:08 +0200

    Play and Pause Audio

--- lib/presentation/pages/player/widgets/controls_btns.dart
+++ lib/presentation/pages/player/widgets/controls_btns.dart
@@ -1,5 +1,29 @@
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
 import 'package:dom_tac_music_player/presentation/pages/player/widgets/player_btn.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
+
+AudioPlayer player = AudioPlayer();
+// Future<Uint8List> audioBytes() async {
+
+//   ByteData bytes = await rootBundle.load(audioAsset);
+//   Uint8List audiobytes =
+//       bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+//   return audiobytes;
+// }
+
+play() async {
+  String audioAsset = 'audio/beatles.mp3';
+  await player.play(
+    AssetSource(audioAsset),
+  );
+}
+
+pause() async {
+  await player.pause();
+}
 
 Widget controlsBtns() {
   return Padding(
@@ -8,7 +32,11 @@ Widget controlsBtns() {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         playerBtn(Icons.shuffle),
-        clickedBtn(Icons.play_arrow),
+        InkWell(
+            onTap: () {
+              play();
+            },
+            child: clickedBtn(Icons.play_arrow)),
         playerBtn(Icons.repeat)
       ],
     ),
